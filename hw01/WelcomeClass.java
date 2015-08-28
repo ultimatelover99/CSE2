691ebbd1a34ac8bc5fffcaed7639a8dda2f8f433
ultimate99 <e.wilkies27@gmail.com>
2015-08-28 18:52:17 +0000

    added hw01 to github

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Emily Wilkinson
+//8/28/15
+//Fouh CSE2
+//
+//compile program
+//run program
+//
+//define class
+public class WelcomeClass {
+    
+//  add main method
+    public static void main (String [] args){
+        
+//  print welcome sign
+
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-E--M--W--3--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("This June my sister and I went to Japan for two weeks.");
+    
+    }//end main method
+}//end class
