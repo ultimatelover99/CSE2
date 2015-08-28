af65ad10bc2f0331cde880acf379f2542f0abb87
ultimate99 <e.wilkies27@gmail.com>
2015-08-28 13:32:49 +0000

    added HelloWorld to github

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+//Emily Wilkinson
+//8/28/15 Hello World
+//Fouh CSE2
+//
+//compile program
+//  javac HelloWorld.java
+//run program
+//  java HelloWorld
+//
+//define class
+public class HelloWorld{
+
+//add main method
+    public static void main (String[] args){
+        
+//print statement with a box around it:
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2015!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    }//end main method
+    
+}//end class
